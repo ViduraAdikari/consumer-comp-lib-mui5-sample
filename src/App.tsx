2f568f70fc46d036df847f60c868d94a9ff4d7b9
Vidura Adikari <vidura.ce@gmail.com>
2022-03-24 12:34:16 +0530

    app name and author

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+// Usage of exported component
+// Project: https://github.com/ViduraAdikari/comp-lib-mui5-sample.git
+// Author: Vidura Adikari
+// Website: https://www.viduraadikari.com/
+
 import React from 'react';
 import {Dashboard} from "comp-lib-mui5-sample";
 
